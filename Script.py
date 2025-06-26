1d26b22cabb7959d1160ba9ff04d3d6e174d0490
Mohit <mohittatu386@gmail.com>
2025-06-26 14:23:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -119,7 +119,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}</b>"""
+    FILE_CAPTION = """<b>{caption}</b>"""
 
     ALRT_TXT = """ᴡʜᴀᴛ ᴀʀᴇ ʏᴏᴜ sᴇᴀʀᴄʜɪɴɢ !?"""
 
